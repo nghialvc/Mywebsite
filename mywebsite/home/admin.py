637a711a2064d95f150b03581b64728e58804a82
nghialvc <70871564+nghialvc@users.noreply.github.com>
2020-09-14 22:04:11 +0700

    Add files via upload

---
+++ mywebsite/home/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from . import models
+
+# Register your models here.
+admin.site.register(models.MangaType)
+admin.site.register(models.MangaInfo)
+admin.site.register(models.ChapInfo)
+admin.site.register(models.MangaContent)
+
